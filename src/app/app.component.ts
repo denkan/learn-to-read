3df79fee70b1ee2ba55e862db72edd6766e331d2
Dennis Nyström <dennis.nystrom@gmail.com>
2022-12-15 19:12:39 +0100

    Add 404 with path redirect + CNAME

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private route: ActivatedRoute, private router: Router) {
+    // if ?p=my/path present, try navigate to it
+    this.route.queryParams
+      .pipe(first((params) => !!params['p']))
+      .subscribe((params) => this.router.navigateByUrl(params['p']));
+  }
+}
